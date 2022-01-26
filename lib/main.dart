3757b28c999ceaa04bf6ddd0cf38074997bb6894
윤지인 <74345861+22000487zixxin@users.noreply.github.com>
2022-01-26 14:49:43 +0900

    debug line 제거

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'project02',
       theme: ThemeData(),
       home: const MyHomePage(title: ''),
